28993a02a22215268f2bfaacd9d75407ad0ab603
duaaaalharbi-ops <duaaaalharbi@gmail.com>
2026-09-12 10:18:10 +0300

    Add files via upload

---
+++ certificates-data.js
@@ -0,0 +1,30 @@
+/* منجز | بيانات الورش الثابتة للشهادات الرقمية
+   هذا الملف لا يحفظ أسماء المستفيدات ولا بيانات إصدار الشهادات
+*/
+window.MANJAZ_CERTIFICATE_WORKSHOPS = [
+  {
+    id: "reciprocal-teaching",
+    title: "التدريس التبادلي",
+    implementers: [
+      "أ. أسماء محمد القحطاني",
+      "أ. سهام عبدالله الغامدي"
+    ],
+    day: "الثلاثاء",
+    date: "١٤٤٨.٣.١٢هـ",
+    durationHours: 2,
+    durationText: "ساعتان",
+    attendanceText: "بواقع ساعتين تدريبيتين"
+  },
+  {
+    id: "classroom-observation-card",
+    title: "إيضاحات حول بطاقة رصد الملاحظة الصفية",
+    implementers: [
+      "أ. أسماء محمد القحطاني"
+    ],
+    day: "الخميس",
+    date: "١٤٤٨.٣.٢٨هـ",
+    durationHours: 2,
+    durationText: "ساعتان",
+    attendanceText: "بواقع ساعتين تدريبيتين"
+  }
+];
